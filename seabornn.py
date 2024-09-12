911c6acd7161ccdbdb9ff39d58276e8d9cf53cc8
charanreddy001 <charanreddypotham111@gmail.com>
2024-09-12 18:52:20 +0530

    Add initial data analysis and plotting scripts
    Integrated basic data analysis with pandas, including descriptive statistics and visualizations, in pandass.py. Additionally, added scripts for line plots, bar graphs, and scatter plots using matplotlib and seaborn in new files matplotlibb.py, matplotlibb2.py, and seabornn.py.Add queue implementation using deque
    
    Introduced a new queue implementation using the `deque` from Python's collections module for better performance. This includes basic queue operations like enqueue, dequeue, and methods to check if the queue is empty and to get its size.

--- seabornn.py
+++ seabornn.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 
 # Specify the file path
 file_path = r'C:\Users\Charan Reddy\Downloads\datasett\customers.csv'
-
 # Load the dataset into a pandas DataFrame
 data = pd.read_csv(file_path)
 
@@ -13,6 +12,15 @@ print(data.head())
 
 # Create a scatterplot using seaborn
 sns.scatterplot(data=data, x='region', y='purchase_frequency')
+plt.show()
 
-# Show the plot
+# this gives the size of figure
+# in this pycharm this decides the size of window
+plt.figure(figsize = (10,7))
+sns.scatterplot(data = data, x = 'age', y = 'annual_income', hue = 'region',size = 'region')
+# you give hue to the column which you want to display in different colours
+plt.show()
+
+
+sns.scatterplot(data = data, x = 'age', y = 'annual_income', hue = 'region',s = 200,alpha =0.6)
 plt.show()
