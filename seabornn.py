8d53087bc631beebc2af16d30f326674153e709a
charanreddy001 <charanreddypotham111@gmail.com>
2024-09-12 19:37:44 +0530

    Add initial data analysis and plotting scripts
    Integrated basic data analysis with pandas, including descriptive statistics and visualizations, in pandass.py. Additionally, added scripts for line plots, bar graphs, and scatter plots using matplotlib and seaborn in new files matplotlibb.py, matplotlibb2.py, and seabornn.py.Add queue implementation using deque
    
    Introduced a new queue implementation using the `deque` from Python's collections module for better performance. This includes basic queue operations like enqueue, dequeue, and methods to check if the queue is empty and to get its size.

--- seabornn.py
+++ seabornn.py
@@ -10,6 +10,8 @@ data = pd.read_csv(file_path)
 # Preview the first few rows of the dataset
 print(data.head())
 
+
+#                                        SCATTER PLOT MAIN FOCUS
 # Create a scatterplot using seaborn
 sns.scatterplot(data=data, x='region', y='purchase_frequency')
 plt.show()
@@ -19,8 +21,13 @@ plt.show()
 plt.figure(figsize = (10,7))
 sns.scatterplot(data = data, x = 'age', y = 'annual_income', hue = 'region',size = 'region')
 # you give hue to the column which you want to display in different colours
+# and also size option to region gives different size to it
 plt.show()
 
 
-sns.scatterplot(data = data, x = 'age', y = 'annual_income', hue = 'region',s = 200,alpha =0.6)
+sns.scatterplot(data = data, x = 'age', y = 'annual_income', hue = 'region',s = 200,alpha =0.6,style = 'region')
+# alpha refers to the transparency with the dots
+# style refers to different shapes of region
 plt.show()
+plt.savefig('seaborne_graph.jpg')
+
